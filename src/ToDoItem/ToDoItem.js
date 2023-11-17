f00228966cd33b2ce54a9e208e06e13f237ae897
goldbaren189e <aa4075jhi@naver.com>
2023-11-18 00:46:45 +0900

    todolist

---
+++ src/ToDoItem/ToDoItem.js
@@ -0,0 +1,13 @@
+import "./ToDoItem.css";
+
+const ToDoItem = ({item, removeToDoItem}) => {
+
+    return (
+        <li>
+            {item}
+            <button onClick={removeToDoItem}>Remove</button>
+        </li>
+    );
+}
+
+export default ToDoItem;
